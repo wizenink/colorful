5cc5c9651705164388b2ab0b16ee69b2b0835665
David Maseda <david.maseda@hotmail.com>
2020-02-25 17:26:43 +0100

    bugfixes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { encode } from 'url-safe-base64';
 import './App.css';
-import { AppBar, Typography, Toolbar,Button,Container} from '@material-ui/core';
+import { AppBar, Typography, Toolbar, Button, Container } from '@material-ui/core';
 import ImageUploader from './ImageUploader';
 import ModelSelector from './ModelSelector';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 
 var endpoint = ""
-if(process.env.REACT_APP_API === undefined)
-{
+if (process.env.REACT_APP_API === undefined) {
   endpoint = "127.0.0.1:5050"
 }
-else
-{
+else {
   endpoint = process.env.REACT_APP_API
 }
 
@@ -26,17 +25,27 @@ class App extends Component {
       original: [],
       originalb64safe: [],
       result: [],
-      models: []
+      models: [],
+      loading : false
     }
   }
 
-  componentDidMount()
-  {
-    axios.get("http://"+endpoint+"/list").then(
+  componentDidMount() {
+    axios.get("http://" + endpoint + "/list").then(
       res => {
-        this.setState({models:res.data})
+        this.setState({ models: res.data })
       }
     ).catch(err => console.log(err))
+  }
+  handleImageDelete(file) {
+    const index = this.state.original.indexOf(file)
+    var o = this.state.original
+    if (index > -1) {
+      o.splice(index, 1);
+    }
+    this.setState({
+      original:o
+    })
   }
   handleImageAdd(file) {
     var reader = new FileReader()
@@ -44,22 +53,31 @@ class App extends Component {
     reader.onloadend = () => {
       this.setState(prevState => ({
         original: [...prevState.original, file],
-        originalb64safe: [...prevState.originalb64safe, encode(reader.result.split(',')[1].replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, ''))]
       }))
     }
   }
-
+  clear()
+  {
+    this.setState({
+      result : []
+    })
+  }
   makeRequests() {
-    this.state.originalb64safe.forEach(element => {
-      axios.post("http://"+endpoint+"/gen", { "image_b64": element }).then(
+    this.state.original.forEach(element => {
+    var reader = new FileReader()
+    reader.readAsDataURL(element)
+    reader.onloadend = () => {
+      var encoded = encode(reader.result.split(',')[1].replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, ''))
+      axios.post("http://" + endpoint + "/gen", { "image_b64": encoded }).then(
         res => {
           this.setState(prevState => ({
             result: [...prevState.result, res.data],
           }))
         }
       ).catch(err => { console.error(err) })
-    });
+    }});
   }
+
   handleImageUpload(file) {
     var reader = new FileReader()
     reader.readAsDataURL(file)
@@ -70,28 +88,35 @@ class App extends Component {
         originalb64safe: encode(reader.result.split(',')[1].replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, ''))
       })
       console.log(process.env.REACT_APP_API)
-      axios.post(process.env.REACT_APP_API,{
-        
-        "image_b64":this.state.originalb64safe}).then(
+      axios.post(process.env.REACT_APP_API, {
 
-      res => {
-        this.setState({result:res.data});
-      }
-    ).catch(err => {console.error(err)})
+        "image_b64": this.state.originalb64safe
+      }).then(
+
+        res => {
+          this.setState({ result: res.data });
+        }
+      ).catch(err => { console.error(err) })
     }
   }
 
   swapModel(model) {
-    axios.post("http://"+endpoint+"/model/"+model.target.value).then(
+    this.setState({
+      loading : true
+    })
+    axios.post("http://" + endpoint + "/model/" + model.target.value).then(
       res => {
         console.log("RES")
         console.log(res)
-        console.log(res.data)}
-    ).catch(err => 
-      {
-        console.log("ERROR")
-        console.log(err)
-      })
+        console.log(res.data)
+        this.setState({
+          loading:false
+        })
+      }
+    ).catch(err => {
+      console.log("ERROR")
+      console.log(err)
+    })
   }
 
   render() {
@@ -102,22 +127,24 @@ class App extends Component {
             <Typography variant="h5" color="inherit">
               Colorful
             </Typography>
-            <ModelSelector models={this.state.models} onChange={this.swapModel}></ModelSelector>
+            <ModelSelector models={this.state.models} onChange={this.swapModel.bind(this)}></ModelSelector>
           </Toolbar>
         </AppBar>
 
-        
-        <ImageUploader onChange={this.handleImageAdd.bind(this)}></ImageUploader>
+
+        <ImageUploader onDelete={this.handleImageDelete.bind(this)} onChange={this.handleImageAdd.bind(this)}></ImageUploader>
         <hr></hr>
-      
-        <Button variant="contained" color="primary" disabled={(this.state.originalb64safe.length===0)} onClick={this.makeRequests.bind(this)}>Colorize</Button>
-       
+
+        <Button variant="contained" color="primary" disabled={(this.state.original.length === 0) && (this.state.loading)} onClick={this.makeRequests.bind(this)}>Colorize</Button>
+        {this.state.loading && <CircularProgress size={30}/>}
+        <Button variant="contained" color="primary" disabled={(this.state.result.length === 0)} onClick={this.clear.bind(this)}>Clear</Button>
+
         <Container>
-        {this.state.result.map(image => {
-          return (<img src={"data:image/png;base64," + image} alt="" />
-          )
-        })
-        }
+          {this.state.result.map(image => {
+            return (<img src={"data:image/png;base64," + image} alt="" />
+            )
+          })
+          }
         </Container>
       </div>
     )
--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -4,7 +4,7 @@ export default class ImageUploader extends Component {
     render() {
         return (
             <div>
-                <DropzoneArea onDrop={this.props.onChange} acceptedFiles={['image/*']} dropzoneText="Drag and drop an image here or select a file to upload">
+                <DropzoneArea onDelete={this.props.onDelete} onDrop={this.props.onChange} acceptedFiles={['image/*']} dropzoneText="Drag and drop an image here or select a file to upload">
                 </DropzoneArea>
             </div>
         )
